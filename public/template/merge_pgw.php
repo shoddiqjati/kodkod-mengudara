57fea52ffed7eabc0213bff2dab44466236761e5
vinsensiussatya <vincentsaty@gmail.com>
2016-05-28 07:30:48 +0700

    nambah dolar

--- public/template/merge_pgw.php
+++ public/template/merge_pgw.php
@@ -317,7 +317,7 @@ if ($result_surat->num_rows>0) {
 
 $nomor_surat = rand(100,199);
 
-$query = "INSERT INTO `record_pgw`(`tanggal_surat`, `pgw_id`, `nama_pgw`, `nama_surat`,`no_surat`, `keterangan`, `status`) VALUES ('$tanggal', '$pgw_id', '$nama_pgw', '$nama_surat','nomor_surat', '$ket', 'Processing')";
+$query = "INSERT INTO `record_pgw`(`tanggal_surat`, `pgw_id`, `nama_pgw`, `nama_surat`,`no_surat`, `keterangan`, `status`) VALUES ('$tanggal', '$pgw_id', '$nama_pgw', '$nama_surat','$nomor_surat', '$ket', 'Processing')";
 $result_insert = $conn->query($query);
 $nomor_surat = rand(100,199);
 
